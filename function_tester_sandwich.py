9b6b5bfd1bc1fdfd592406fe098ed9a912ab1928
virginie.desharnais <virginie.desharnais@Digital-Grenoble.local>
2022-04-15 11:00:21 +0200

    Foutu Sandwich

--- function_tester_sandwich.py
+++ function_tester_sandwich.py
@@ -5,10 +5,13 @@ dic_dir = {"DHG":9, "DHD":7, "DBG":-7, "DBD":-9, "H":8, "B":-8, "D":-1, "G":1}
 def est_sandwich(df_jeu, lig, col, case, direction) :
     i =1
     flag_sand = False
+    flag_autre_case = False
     pas = dic_dir[direction]
     pos_case = 8 * (lig-1) + col -1
-    while ((pos_case - pas * i)%8 != 0 and (pos_case - pas * i)%8 != 7) and flag_sand == False:
-        if df_jeu.iloc[ pos_case - pas * i].values[2] == case :
+    while (pos_case - pas * i > 0 and pos_case - pas * i<63 and (pos_case - pas * i)%8 != 0 and (pos_case - pas * i)%8 != 7) and flag_sand == False:
+        if df_jeu.iloc[ pos_case - pas * i].values[2] != case and df_jeu.iloc[ pos_case - pas * i].values[2] != "dispo" :
+            flag_autre_case = True
+        if flag_autre_case == True and df_jeu.iloc[ pos_case - pas * i].values[2] == case :
             flag_sand = True
         i = i + 1
     
@@ -22,3 +25,30 @@ def tester_sandwich(echiquier,lig,col,case) :
             break
     return False
 
+def tester_sandwich_possible(echiquier, case):
+    flag_sand = False
+    req = 'cases == "dispo"'
+    list_jeu_cases_vides = echiquier.query('cases == "dispo"').index.values.tolist()
+    for elem in list_jeu_cases_vides:
+        s = echiquier.iloc[elem][["lig","col"]]
+        if est_case_adj_contraire(echiquier,s[0],s[1],case) == True:
+            if tester_sandwich(echiquier,s[0], s[1],case) == True:
+                print(s[0], s[1],"=> sandwich possible")
+                flag_sand == True
+    return flag_sand
+
+def est_case_adj_vide(df_jeu, lig, col):
+    req = '(lig == ' + str(lig) + ' and col == ' + str(col - 1) + ' and cases != "dispo") or (lig == ' + str(lig) + ' and col == ' + str(col + 1) + ' and cases != "dispo") or (lig == ' + str(lig - 1) + ' and col == ' + str(col) + ' and cases != "dispo") or (lig == ' + str(lig + 1) + ' and col == ' + str(col) + ' and cases != "dispo")'
+    df_jeu_cases_adj = df_jeu.query(req).iloc[:]["cases"].copy()
+    if len(df_jeu_cases_adj) == 0:
+        return True
+    else :
+        return False
+
+def est_case_adj_contraire(df_jeu, lig, col, case):
+    req = 'cases != "' + case + '" and cases != "dispo" and ((lig == ' + str(lig) + ' and col == ' + str(col - 1) + ') or (lig == ' + str(lig) + ' and col == ' + str(col + 1) + ') or (lig == ' + str(lig - 1) + ' and col == ' + str(col) + ') or (lig == ' + str(lig + 1) + ' and col == ' + str(col) + '))'
+    df_jeu_cases_adj = df_jeu.query(req).iloc[:]["cases"].copy()
+    if len(df_jeu_cases_adj) != 0:
+        return True
+    else :
+        return False
--- othello.py
+++ othello.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import function_tester_sandwich as fts
+import Partie_init as init
 
 
-def verifier_coup(df_jeu, lig, col, case) : 
+def verifier_coup(df_jeu, lig, col, case, cas_passe="") : 
     flag_coup_ok = True
     
     # Teste si les coordonnées en ligne et colonne sont sur l'échiquier
@@ -10,34 +11,37 @@ def verifier_coup(df_jeu, lig, col, case) :
         print("Erreur Numéro de ligne - Coup joué hors de l'échiquier")
         flag_coup_ok = False
     
-    if col >9 or col <1 :
+    if flag_coup_ok == True and (col >9 or col <1) :
         print("Erreur Numéro de Colonne - Coup joué hors de l'échiquier")
         flag_coup_ok = False
 
     # Teste si la case est déjà jouée
     req = 'lig == ' + str(lig) + ' and col == ' + str(col)
     case_testee = df_jeu.query(req).iloc[0]["cases"]
-    if case_testee != "dispo" :
+    if flag_coup_ok == True and case_testee != "dispo" :
         print("Coup déjà joué")
         flag_coup_ok = False
 
     # teste si la case est voisine d'une case déjà jouée
-    req = '(lig == ' + str(lig) + ' and col == ' + str(col - 1) + ' and cases != "dispo") or (lig == ' + str(lig) + ' and col == ' + str(col + 1) + ' and cases != "dispo") or (lig == ' + str(lig - 1) + ' and col == ' + str(col) + ' and cases != "dispo") or (lig == ' + str(lig + 1) + ' and col == ' + str(col) + ' and cases != "dispo")'
-    df_jeu_cases_adj = df_jeu.query(req).iloc[:]["cases"].copy()
-    if len(df_jeu_cases_adj) == 0:
+    if flag_coup_ok == True and fts.est_case_adj_vide(df_jeu,lig,col) == True:
         print("case non voisine d'une case jouée")
         flag_coup_ok = False
+    
+    # Teste le cas "Passe ton tour"
+    if flag_coup_ok == True and cas_passe == "passe":
+        # On vérifie qu'il n'y a pas de sandwich possible
+        if fts.tester_sandwich_possible(df_jeu,case) == True:
+            print("Passe ton tour - Pas de sandwich possible")
+            flag_coup_ok = False
 
     # Teste si la case jouée permet de faire un sandwich
-    if flag_coup_ok != False and fts.tester_sandwich(df_jeu, lig, col, case) == False :
+    if flag_coup_ok != False and cas_passe != "passe" and fts.tester_sandwich(df_jeu, lig, col, case) == False :
         print("Il n' y a pas de sandwich pour ce coup")
-        flag_coup_ok = false 
+        flag_coup_ok = False 
 
+    
     return flag_coup_ok
 
-#verifier_coup(echiquier, 3,4,"blanc")
-
-
-
+verifier_coup(init.echiquier, 3,4,"blanc","passe")
 
 
